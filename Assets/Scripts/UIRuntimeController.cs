a896e1e35eab13b6a5046bcd9bd05d48e456a4d3
dblnjsw <1532611298@qq.com>
2021-06-15 17:39:04 +0800

    tw7bcd

--- Assets/Scripts/UIRuntimeController.cs
+++ Assets/Scripts/UIRuntimeController.cs
@@ -1,6 +1,10 @@
 ﻿using FairyGUI;
+using LitJson;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class UIRuntimeController : MonoBehaviour
 {
@@ -8,7 +12,11 @@ public class UIRuntimeController : MonoBehaviour
     public List<GameObject> panels;
     Dictionary<string, GameObject> dic_panels;
 
-    GComponent mainView;
+    GComponent mainView, mainView_login;
+
+    //login
+    string login_username, login_password;
+    string login_url = "http://127.0.0.1:8083/login2";
 
     // Start is called before the first frame update
     void Start()
@@ -23,30 +31,97 @@ public class UIRuntimeController : MonoBehaviour
 
         //Research
 
-        mainView = dic_panels["Panel_Research"].GetComponent<UIPanel>().ui;
+        //mainView = dic_panels["Research"].GetComponent<UIPanel>().ui;
 
-        GTree list = mainView.GetChild("list").asTree;
-        GTreeNode rootNode = list.rootNode;
+        //GTree list = mainView.GetChild("list").asTree;
+        //GTreeNode rootNode = list.rootNode;
 
-        foreach (string key in researchTree.Keys)
+        //foreach (string key in researchTree.Keys)
+        //{
+        //    GComponent list_item0 = list.GetFromPool("ui://5eyvfdebvxu0o").asCom;
+
+        //    //rootNode.AddChild(list_item0);
+
+        //    list_item0.GetChild("text").asTextField.text = key;
+        //    list.AddChild(list_item0);
+        //    foreach (string it in researchTree[key])
+        //    {
+        //        GComponent list_item1 = list.GetFromPool("ui://5eyvfdeblfbcl").asCom;
+        //        list_item1.GetChild("text").asTextField.text = it;
+        //        list.AddChild(list_item1);
+
+        //    }
+        //}
+
+        //Login
+        mainView_login = dic_panels["Login"].GetComponent<UIPanel>().ui;
+
+        GObject btn_cancel = mainView_login.GetChild("cancel");
+        btn_cancel.onClick.Add(() =>
         {
-            GComponent list_item0 = list.GetFromPool("ui://5eyvfdebvxu0o").asCom;
+            mainView_login.visible = false;
+        });
 
-            //rootNode.AddChild(list_item0);
+        GObject btn_login = mainView_login.GetChild("btn_login");
+        btn_login.onClick.Add(onClickLogin);
 
-            list_item0.GetChild("text").asTextField.text = key;
-            list.AddChild(list_item0);
-            foreach (string it in researchTree[key])
-            {
-                GComponent list_item1 = list.GetFromPool("ui://5eyvfdeblfbcl").asCom;
-                list_item1.GetChild("text").asTextField.text = it;
-                list.AddChild(list_item1);
 
-            }
+    }
+
+    IEnumerator UnityWebRequestPost(string url, byte[] postBytes)
+    {
+        UnityWebRequest request = new UnityWebRequest(url, "POST");
+
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(postBytes);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+        Debug.Log("Status Code: " + request.responseCode);
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.LogError(request.error);
         }
+        else
+        {
+            Debug.Log(request.downloadHandler.text);
+        }
+    }
+    void onClickLogin()
+    {
+        var task = requestLogin();
+        Debug.Log("start coroutine");
+    }
+    private async Task requestLogin()
+    {
+        login_username = mainView_login.GetChild("input_username").text;
+        login_password = mainView_login.GetChild("input_password").text;
+
+        JsonData data = new JsonData();
+        data["username"] = login_username;
+        data["password"] = login_password;
+        byte[] postBytes = System.Text.Encoding.Default.GetBytes(data.ToJson());
+        //UnityWebRequestPost(, postBytes);
+        UnityWebRequest www = UnityWebRequest.Get(login_url);
+        UnityWebRequestAsyncOperation request = www.SendWebRequest();
+
+        while (!request.isDone)
+            await Task.Delay(100);
+        
 
 
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
 
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+        Debug.Log("username:" + login_username);
     }
 
     // Update is called once per frame
